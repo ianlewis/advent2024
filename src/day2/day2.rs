2a8bb3f844ac318d5ca7dc70447a491535eb6dc0
Ian Lewis <ianmlewis@gmail.com>
2024-12-22 00:09:50 +0000

    feat: Add day2 part2 solution
    Signed-off-by: Ian Lewis <ianmlewis@gmail.com>

--- src/day2/day2.rs
+++ src/day2/day2.rs
@@ -68,9 +68,36 @@ use std::str;
 
 // Program day2 reads the input on stdin and prints the total number of safe reports.
 
+fn is_safe(list: Vec<i64>, increasing: bool, skip: usize) -> usize {
+    let mut prev = 0;
+    for (i, n) in list.iter().enumerate() {
+        if i == skip && skip != 0 {
+            continue;
+        }
+        // Check for a diff 0 < x < 3
+        if i > 0 {
+            let diff = (*n - prev).abs();
+            if diff < 1 || diff > 3 {
+                return i;
+            }
+        }
+        if i > 1 {
+            // Check increasing/descreasing
+            if (increasing && *n < prev) || (!increasing && *n > prev) {
+                return i;
+            }
+        }
+
+        prev = *n;
+    }
+
+    return 0;
+}
+
 fn run(r: impl BufRead) -> Result<(i64, i64), String> {
     // Read in both lists.
     let mut safe_num = 0;
+    let mut semi_safe_num = 0;
     for line in r.lines() {
         if let Err(e) = line {
             return Err(e.to_string());
@@ -79,44 +106,53 @@ fn run(r: impl BufRead) -> Result<(i64, i64), String> {
         let line_str = line.unwrap();
         let iter = str::split_whitespace(&line_str);
 
-        let mut increasing = true;
-        let mut safe = true;
-        let mut prev = 0;
-        for (i, v) in iter.enumerate() {
+        let mut list = Vec::new();
+        for v in iter {
             let n = v.parse::<i64>().map_err(|err| err.to_string())?;
-            // Check for a diff 0 < x < 3
-            if i > 0 {
-                let diff = (n - prev).abs();
-                if diff < 1 || diff > 3 {
-                    safe = false;
-                    break;
-                }
-            }
-            if i == 1 {
-                // determine if increasing or decreasing
-                increasing = n > prev;
-            } else if i > 1 {
-                // Check increasing/descreasing
-                if (increasing && n < prev) || (!increasing && n > prev) {
-                    safe = false;
-                    break;
-                }
-            }
-
-            prev = n;
+            list.push(n);
         }
 
-        if safe {
+        if list.len() < 2 {
             safe_num += 1;
+            continue;
+        }
+
+        let increasing = list[0] < list[1];
+        // TODO: Don't clone list.
+        let index = is_safe(list.clone(), increasing, 0);
+        if index == 0 {
+            safe_num += 1;
+            semi_safe_num += 1;
+            continue;
+        }
+
+        if index < 3 {
+            // Check if either the first or second value can be removed.
+            if list.len() >= 3 {
+                if is_safe(list[1..].to_vec(), list[1] < list[2], 0) == 0 {
+                    semi_safe_num += 1;
+                    continue;
+                } else if is_safe(list.clone(), list[0] < list[2], 1) == 0 {
+                    semi_safe_num += 1;
+                    continue;
+                }
+            }
+        }
+        if index > 1 {
+            // Try skipping the level at index.
+            let index2 = is_safe(list[index - 1..].to_vec(), increasing, 1);
+            if index2 == 0 {
+                semi_safe_num += 1;
+            }
         }
     }
 
-    return Ok((safe_num, 0));
+    return Ok((safe_num, semi_safe_num));
 }
 
 fn main() -> ExitCode {
     let stdin = io::stdin();
-    let (safe_num, _) = match run(stdin.lock()) {
+    let (safe_num, semi_safe_num) = match run(stdin.lock()) {
         Ok((d, s)) => (d, s),
         Err(e) => {
             println!("error running: {e:?}");
@@ -125,6 +161,97 @@ fn main() -> ExitCode {
     };
 
     println!("{}", safe_num);
+    println!("{}", semi_safe_num);
 
     ExitCode::SUCCESS
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use bytes::{Buf, Bytes};
+
+    #[test]
+    fn test_run() -> Result<(), String> {
+        let input = Bytes::from(
+            "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+",
+        );
+
+        let (safe, semi_safe) = run(input.reader())?;
+        assert_eq!(safe, 2);
+        assert_eq!(semi_safe, 4);
+        Ok(())
+    }
+
+    #[test]
+    fn test_unsafe() -> Result<(), String> {
+        let input = Bytes::from(
+            "1 2 7 8 9
+9 7 6 2 1
+",
+        );
+
+        let (safe, semi_safe) = run(input.reader())?;
+        assert_eq!(safe, 0);
+        assert_eq!(semi_safe, 0);
+        Ok(())
+    }
+
+    #[test]
+    fn test_semi_safe_first_val() -> Result<(), String> {
+        let input = Bytes::from(
+            "5 0 1 2 3
+",
+        );
+
+        let (safe, semi_safe) = run(input.reader())?;
+        assert_eq!(safe, 0);
+        assert_eq!(semi_safe, 1);
+        Ok(())
+    }
+
+    #[test]
+    fn test_semi_safe_last_val() -> Result<(), String> {
+        let input = Bytes::from(
+            "1 2 3 4 0
+",
+        );
+
+        let (safe, semi_safe) = run(input.reader())?;
+        assert_eq!(safe, 0);
+        assert_eq!(semi_safe, 1);
+        Ok(())
+    }
+
+    #[test]
+    fn test_semi_safe_direction_first_val() -> Result<(), String> {
+        let input = Bytes::from(
+            "1 0 1 2 3
+",
+        );
+
+        let (safe, semi_safe) = run(input.reader())?;
+        assert_eq!(safe, 0);
+        assert_eq!(semi_safe, 1);
+        Ok(())
+    }
+
+    #[test]
+    fn test_semi_safe_direction_last_val() -> Result<(), String> {
+        let input = Bytes::from(
+            "0 1 2 3 2
+",
+        );
+
+        let (safe, semi_safe) = run(input.reader())?;
+        assert_eq!(safe, 0);
+        assert_eq!(semi_safe, 1);
+        Ok(())
+    }
 }
