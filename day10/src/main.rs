d7728c0c69315f9172b07d4c08e48c1522eb1695
Ian Lewis <IanMLewis@gmail.com>
2025-01-08 16:29:13 +0900

    feat: Day 10 part 1 solution (#12)
    Signed-off-by: Ian Lewis <ianmlewis@gmail.com>

---
+++ day10/src/main.rs
@@ -0,0 +1,144 @@
+// Copyright 2025 Ian Lewis
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::collections;
+use std::error;
+use std::io::{self, BufRead};
+use std::process;
+
+fn read_map(r: impl BufRead) -> Result<Vec<Vec<Option<i64>>>, Box<dyn error::Error>> {
+    let mut map = Vec::new();
+    for line in r.lines() {
+        let mut col = Vec::new();
+        for c in line?.chars() {
+            if c.is_numeric() {
+                let n = String::from(c).parse::<i64>()?;
+                col.push(Some(n));
+            } else {
+                col.push(None);
+            }
+        }
+
+        map.push(col);
+    }
+
+    Ok(map)
+}
+
+fn find_trails(map: &[Vec<Option<i64>>], x: usize, y: usize) -> usize {
+    let mut trails = collections::HashSet::new();
+
+    let dirs: [(isize, isize); 4] = [
+        (0, -1), // up
+        (0, 1),  // down
+        (-1, 0), // left
+        (1, 0),  // right
+    ];
+
+    let mut stack: collections::VecDeque<(usize, usize, i64)> = collections::VecDeque::new();
+    stack.push_back((x, y, 0));
+
+    while !stack.is_empty() {
+        let (cur_x, cur_y, expected_num) = stack.pop_back().unwrap();
+        if map[cur_y][cur_x] != Some(expected_num) {
+            continue;
+        }
+        if expected_num == 9 {
+            trails.insert((cur_x, cur_y));
+            continue;
+        }
+
+        for (dx, dy) in dirs {
+            let next_x_o = cur_x.checked_add_signed(dx);
+            let next_y_o = cur_y.checked_add_signed(dy);
+
+            if next_y_o.is_none() {
+                continue;
+            }
+            let next_y = next_y_o.unwrap();
+            if next_y >= map.len() {
+                continue;
+            }
+
+            if next_x_o.is_none() {
+                continue;
+            }
+            let next_x = next_x_o.unwrap();
+            if next_x >= map[next_y].len() {
+                continue;
+            }
+
+            stack.push_back((next_x, next_y, expected_num + 1));
+        }
+    }
+
+    trails.len()
+}
+
+fn score_trailheads(map: &[Vec<Option<i64>>]) -> usize {
+    let mut score = 0;
+
+    for (y, col) in map.iter().enumerate() {
+        for (x, _loc) in col.iter().enumerate() {
+            score += find_trails(map, x, y);
+        }
+    }
+
+    score
+}
+
+fn run(r: impl BufRead) -> Result<usize, Box<dyn error::Error>> {
+    let map = read_map(r)?;
+    Ok(score_trailheads(&map))
+}
+
+fn main() -> process::ExitCode {
+    let stdin = io::stdin();
+    let n = match run(stdin.lock()) {
+        Ok(n) => n,
+        Err(e) => {
+            println!("error running: {e:?}");
+            return process::ExitCode::from(1);
+        }
+    };
+
+    println!("{}", n);
+
+    process::ExitCode::SUCCESS
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use bytes::{Buf, Bytes};
+
+    #[test]
+    fn test_run() -> Result<(), Box<dyn error::Error>> {
+        let input = Bytes::from(
+            "89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
+",
+        );
+
+        let n = run(input.reader())?;
+        assert_eq!(n, 36);
+        Ok(())
+    }
+}
